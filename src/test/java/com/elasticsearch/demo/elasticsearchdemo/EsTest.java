35b624aa3106c3aa2c61a14f1eef67a3932ee6e6
Mr.Fu <896930409@qq.com>
2020-09-28 14:12:03 +0800

    添加新增文档测试案例

--- src/test/java/com/elasticsearch/demo/elasticsearchdemo/EsTest.java
+++ src/test/java/com/elasticsearch/demo/elasticsearchdemo/EsTest.java
@@ -2,20 +2,26 @@ package com.elasticsearch.demo.elasticsearchdemo;
 
 import com.elasticsearch.demo.elasticsearchdemo.entity.Book;
 import com.elasticsearch.demo.elasticsearchdemo.repository.BookRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.annotation.Resource;
 import java.util.Date;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class EsTest {
 
     @Autowired
     private BookRepository bookRepository;
 
+    /**
+     * ES新增文档案例
+     */
+    @Test
     public void test1(){
-//        String name, String title, int page, Date date
         Book book = new Book("小瓜瓜","这是一个小瓜瓜",1111,new Date());
         bookRepository.save(book);
     }
