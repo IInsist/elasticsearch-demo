b66a2b3ce3f86bdfcb324d13a42839894eb5e909
Mr.Fu <896930409@qq.com>
2020-10-09 11:09:33 +0800

    添加条件查找学生信息方法

--- src/main/java/com/elasticsearch/demo/module/student/service/StudentQdInfoServiceI.java
+++ src/main/java/com/elasticsearch/demo/module/student/service/StudentQdInfoServiceI.java
@@ -1,7 +1,10 @@
 package com.elasticsearch.demo.module.student.service;
 
+import com.elasticsearch.demo.module.student.entity.StudentDoc;
 import com.elasticsearch.demo.module.student.entity.StudentQdInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +16,10 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface StudentQdInfoServiceI extends IService<StudentQdInfo> {
 
+    /**
+     * 姓名模糊查询
+     * @param name
+     * @return
+     */
+    List<StudentDoc> findStudentInfoByName(String name);
 }
