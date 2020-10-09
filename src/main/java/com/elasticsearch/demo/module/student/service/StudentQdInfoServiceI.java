ed5573ab3d8611018dd0c9540c87973e6ff9f8dd
Mr.Fu <896930409@qq.com>
2020-10-09 15:18:03 +0800

    添加相关查找数据案例

--- src/main/java/com/elasticsearch/demo/module/student/service/StudentQdInfoServiceI.java
+++ src/main/java/com/elasticsearch/demo/module/student/service/StudentQdInfoServiceI.java
@@ -3,6 +3,8 @@ package com.elasticsearch.demo.module.student.service;
 import com.elasticsearch.demo.module.student.entity.StudentDoc;
 import com.elasticsearch.demo.module.student.entity.StudentQdInfo;
 import com.baomidou.mybatisplus.extension.service.IService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -22,4 +24,20 @@ public interface StudentQdInfoServiceI extends IService<StudentQdInfo> {
      * @return
      */
     List<StudentDoc> findStudentInfoByName(String name);
+
+    /**
+     * 查找某学院下学生信息
+     * @param xy
+     * @return
+     */
+    List<StudentDoc> findStudentInfoByXy(String xy);
+
+    /**
+     * 通过姓名分页查找学生信息
+     * @param name
+     * @param page
+     * @return
+     */
+    Page<StudentDoc> findStudentInfoByPage(String name, Pageable page);
+
 }
